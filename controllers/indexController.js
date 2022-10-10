f027bcb799d6912f5265fc8a103e4007c68e7632
Tedrosgh <tedrosgh@yahoo.com>
2022-10-10 08:48:59 -0600

    cors policy

---
+++ controllers/indexController.js
@@ -0,0 +1,5 @@
+const getIndex=(req, res)=>{
+    res.render('index', {title: 'Express'});
+};
+
+export default getIndex;
--- index.js
+++ index.js
@@ -5,28 +5,34 @@ import postRoutes from "./routes/postsRoutes.js";
 import dotenv from "dotenv";
 import userRoutes from "./routes/users.js";
 import mezmursRouter from "./routes/mezmurs.js";
+import indexRouter from "./routes/index.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
 //import http from "http";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log("directorz name", __dirname);
 
 const app = express();
 dotenv.config();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-//const PORT = http.createServer(process.env.PORT || 8000);
 const PORT = process.env.PORT || 8000;
 app.use(cors({
   'Access-Contol-Allow-Origin': '*',
   'Access-Contol-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
   'Access-Contol-Allow-Headers': 'Content-Type, Access-Contol-Allow-Headers, x-test'
-//  origin: "http://localhost:3000",
 }));
 
-//http://localhost:5000/posts
 app.get("/", (req, res) => {
   res.send("Hello World!!, That is the new idea");
 });
 
+app.use('/', indexRouter);
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 app.use("/mezmur", mezmursRouter);
@@ -40,7 +46,7 @@ mongoose
   })
   .then(() =>
     app.listen(PORT, () =>
-      console.log(`Server Running on Port: http://localhost:${PORT} and mongoDb on Port ${mongoose.connection.port} or ${process.env.CONNECTION_URL}`)
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
     )
   )
   .catch((error) => console.log(`${error} is not connect`));
